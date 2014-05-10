98274ba6e6ea8885497e52d7ef1ec12e047b54e3
alice.grange05 <grangea@users.noreply.github.com>
2014-05-10 18:02:48 +0000

    Modification affichage liste résultats des parcours

--- public/javascripts/sefaireconduire.js
+++ public/javascripts/sefaireconduire.js
@@ -12,9 +12,30 @@ $( document ).ready(function() {
 
 function searchTravel(){
     //TODO Recuperer les champs du form
+
+    var list = document.getElementsByClassName("ui divided list")[0];
+    $(".ui divided list .item").remove();
+
     $.ajax( "/tousLesParcours" )
         .done(function(data) {
-            console.log(data);
+            $.each(data, function (key, value) {
+                var newdivitem = document.createElement('div');
+                newdivitem.setAttribute('class', 'item');
+                var newdivbutton = document.createElement('div');
+                newdivbutton.setAttribute('class', 'right floated tiny teal ui button');
+                newdivbutton.innerHTML = "Voir";
+                var newdivcontent = document.createElement('div');
+                newdivcontent.setAttribute('class', 'content');
+                var newdivheader = document.createElement('div');
+                newdivheader.setAttribute('class', 'header');
+                newdivheader.innerHTML = value.dep.nom + ' ' +  value.arr.nom + ' ' + value.dateParcours;
+
+                list.appendChild(newdivitem);
+                newdivitem.appendChild(newdivbutton);
+                newdivitem.appendChild(newdivcontent);
+                newdivcontent.appendChild(newdivheader);
+            });
+
         })
         .fail(function(error) {
             console.log("error");

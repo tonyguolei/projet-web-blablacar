01a38726f53eb11d40f51792422e4fa91dc5dc07
alice.grange05 <grangea@users.noreply.github.com>
2014-05-13 21:03:59 +0000

    Ajout formulaire d'inscription

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,3 +5,19 @@
  * Time: 21:57
  * To change this template use File | Settings | File Templates.
  */
+
+$(document).bind("ready", function () {
+    $('.modal')
+        .modal('hidden');
+    ;
+    $("#boutonSeconnecter").bind("click", connectUser);
+    $("#boutonSinscrire").bind("click", register);
+});
+
+function register(){
+    $('.modal').modal('show');
+}
+
+function connectUser(){
+    alert("connexion user");
+}

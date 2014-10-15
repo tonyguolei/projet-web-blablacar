894f097b65110150314fca5a4e5b61473685f288
tonyguolei <tonyguolei@gmail.com>
2014-10-15 20:58:51 +0000

    restructure the project

---
+++ 3A/application repartie/appli_repartie/Users/src/main/java/main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by tonyguolei on 10/13/2014.
+ */
+public class main {
+    public static void main(String[] args){
+
+        System.out.print("Hello World !!!");
+        System.out.println("Hello Guo lei and Grange Alice！！");
+    }
+}

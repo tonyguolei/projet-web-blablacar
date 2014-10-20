617c29cbf0fc621751dc44e64e1cea349f9f3afd
tonyguolei <tonyguolei@gmail.com>
2014-10-20 15:12:28 +0000

    test

--- 3A/application repartie/appli_repartie/Users/src/main/java/User.java
+++ 3A/application repartie/appli_repartie/Users/src/main/java/User.java
@@ -7,8 +7,20 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class User {
-    public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 10000);
+
+    // les paremetres d'user
+    private String pseudo;
+    private String addressServer;
+    private int port;
+    private Socket socket;
+
+    //constructeur
+    public User(String addressServer, int port) throws Exception{
+
+        this.addressServer = addressServer;
+        this.port = port;
+
+        socket = new Socket(addressServer, port);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -23,5 +35,43 @@ public class User {
             System.out.println(in.readLine());
         }
         socket.close();
+    }
+
+    //les methodes de'User
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getAddressServer() {
+        return addressServer;
+    }
+
+    public void setAddressServer(String addressServer) {
+        this.addressServer = addressServer;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void createPseudo(){
+
+    }
+
+    public void connectServer(){
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new User("localhost", 10000);
     }
 }
